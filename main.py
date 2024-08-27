21de96d6a125649f582adb3c34b71f4454a5c644
h$.weird <dmanh20112000@gmail.com>
2024-08-27 15:56:24 +0700

    add static file

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import time
 import uvicorn
 from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from .database import SessionLocal
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory='templates')
 
 def get_database():
     db = SessionLocal()
@@ -25,6 +32,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 # Middleware
 async def add_process_time_header(request: Request):
     print("in add_process_time_header middleware.") # dummy message
@@ -36,6 +44,11 @@ async def add_process_time_header(request: Request):
 async def app_root():
     return { 'message': 'Helloworld' }
 
+@app.get('/home', response_class=HTMLResponse)
+async def render_home(request: Request) :
+    return templates.TemplateResponse(
+        request=request, name='home.html', context={ 'message': 'Hello World' }
+    )
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8888)
